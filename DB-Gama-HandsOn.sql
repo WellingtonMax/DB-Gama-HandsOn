7fe71fd4fd23631e5752c3035aa0ff8deacfd93b
WellingtonMax <wellington.psi@hotmail.com>
2022-02-09 18:05:59 -0300

    Commit inicial do SQL, tables adicionados, clientes adicionados

---
+++ DB-Gama-HandsOn.sql
@@ -0,0 +1,22 @@
+-- INSERT INTO cliente VALUES 
+-- (null, "Adriano Ribeiro Costa", "adriribeco@gmail.com" , "adricoribe1" , 11111111111),
+-- (null, "Allana Lohanne", "alanne@gmail.com" , "allana221" , 22222222222),
+-- (null, "Maria Joana Silva", "mariajoana@gmail.com" , "majusi1" , 33333333333),
+-- (null, "Caio Cauã", "caio.ca1@gmail.com" , "caca199" , 44444444444),
+-- (null, "Wesley Silva Santos", "wesleysantos21@gmail.com" , "0184wes!" , 55555555555),
+-- (null, "Jandira Savoreti Sampaio", "jandirasavo_sam1@hotmail.com" , "sampa123" , 66666666666),
+-- (null, "Cassio Solano Schmitt", "schmitt_sol@gmail.com" , "cassio9183" , 77777777777),
+-- (null, "Aluisio Ribeiro Souza", "aluisio_rib@gmail.com" , "Alurib25" , 88888888888),
+-- (null, "Luciano Carneiro", "luciano.carneiro@gmail.com" , "lucicar331" , 99999999999),
+-- (null, "Audrey Oliveira", "aaudrey@gmail.com" , "audrey29!" , 10101011010);
+
+SELECT * FROM cliente;
+
+
+
+
+
+
+
+
+
