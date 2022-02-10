26b4fdd89f135e8399c67e94450b67d0099e6465
littate <pedrosilvestrejf@gmail.com>
2022-02-09 22:34:36 -0300

    primeira parte dos pedidos e lista de itens

---
+++ item_pedido.sql
@@ -0,0 +1,5 @@
+INSERT INTO item_pedido VALUES (null, 1, 1, 1, 20000, 20000);
+INSERT INTO item_pedido VALUES (null, 2, 16, 1, 5900, 5900);
+INSERT INTO item_pedido VALUES (null, 3, 38, 1, 3800, 3800);
+INSERT INTO item_pedido VALUES (null, 4, 39, 1, 3300, 3300);
+INSERT INTO item_pedido VALUES (null, 5, 44, 1, 85, 85);
---
+++ pedido.sql
@@ -0,0 +1,5 @@
+INSERT INTO pedido VALUES (null, '01-01-2021', 20000, 'f', 0, 20000, 1);
+INSERT INTO pedido VALUES (null, '22-01-2021', 5900, 'f', 0, 5900, 1);
+INSERT INTO pedido VALUES (null, '12-02-2021', 3800, 'f', 0, 3800, 5);
+INSERT INTO pedido VALUES (null, '20-03-2021', 3300, 'f', 0, 3300, 4);
+INSERT INTO pedido VALUES (null, '01-01-2021', 85, 'f', 0, 85, 1);
