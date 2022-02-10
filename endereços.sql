1874dff3dd9e9b3ca0dc5eeb017592d52783afec
WellingtonMax <wellington.psi@hotmail.com>
2022-02-09 22:11:34 -0300

    Adicionando lista de endereços

---
+++ endereços.sql
@@ -0,0 +1,54 @@
+-- CREATE TABLE endereco(
+
+-- num_seq    INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+
+-- tipo       VARCHAR(5) NOT NULL, 
+
+-- logradouro VARCHAR(45) NOT NULL, 
+
+-- numero     INTEGER, 
+
+-- complemento VARCHAR(20), 
+
+-- bairro     VARCHAR(30), 
+
+-- cidade     VARCHAR(50),
+
+--     estado     VARCHAR(2), 
+
+-- cep        VARCHAR(10), 
+
+-- cliente_id INTEGER NOT NULL,
+
+-- CONSTRAINT endereco_cliente FOREIGN KEY 
+
+-- (cliente_id) REFERENCES cliente(id)
+
+-- );
+
+
+INSERT INTO endereco VALUES (NULL, 'Avenida', 'Rua Beija Flor', 3586, 'Arniqueiras', 'centro', 'Rio de Janeiro', '68952-147', 'RJ', 1);
+INSERT INTO endereco VALUES (NULL, 'Rua', 'Rua Arniqueiras', 7826, 'São Sebatião', 'centro', 'São Paulo', '72585-774', 'SP', 2);
+INSERT INTO endereco VALUES (NULL, 'Avenida', 'Rua Batista de Oliveira', 4432, 'Santo Antonio', 'centro', 'Belo Horizonte', '29535-980', 'MG', 3);
+INSERT INTO endereco VALUES (NULL, 'Rua', 'Rua Desdeth Salgado', 342, null, 'São Mateus', 'Santos Dumont', '45634-333', 'SP', 4);
+INSERT INTO endereco VALUES (NULL, 'Rua', 'Rua Marcelino Pão e Vinho', 33, null, 'Alto dos Passos', 'Lagoa Dourada', '12357-434', 'DF', 5);
+INSERT INTO endereco VALUES (NULL, 'Avenida', 'Avenida Dr. Romualdo', 666, 'apt 3001 bloco A', 'centro', 'Osasco', '54356-876', 'RJ', 6);
+INSERT INTO endereco VALUES (NULL, 'Rua', 'travessa Renato Leite', 49, null, 'Benfica', 'Rio de Janeiro', '34546-785', 'RS', 7);
+INSERT INTO endereco VALUES (NULL, 'Avenida', 'Avenida Rio Branco', 3652, 'apt 202', 'Bom Pastor', 'Juiz de fora', '12345-745', 'BA', 8);
+INSERT INTO endereco VALUES (NULL, 'Rua', 'Travessa Pinheiro Dourado', 14, 'Rua 3', 'Vila Esperança', 'Belo Horizonte', '98556-974', 'MT', 9);
+INSERT INTO endereco VALUES (NULL, 'Avenida', 'Avenida Alagados', 32, 'Apartamento 122', 'Santa Maria', 'Brasília', '98556-974', 'DF', 10);
+INSERT INTO endereco VALUES (NULL, 'Avenida', 'Setor Industrial', 20, 'Apartamento 212', 'Gama', 'Brasília', '98556-974', 'DF', 11);
+INSERT INTO endereco VALUES (NULL, 'Rua', 'Rua Macaxeira', 851, 'casa 38', 'Vila Esperança', 'Belo Horizonte', '98556-974', 'MT', 12);
+INSERT INTO endereco VALUES (NULL, 'Rua', 'Rua Canarinho', 144, ' 27', 'Vila Esperança', 'Belo Horizonte', '98556-974', 'MT', 13);
+INSERT INTO endereco VALUES (NULL, 'Rua', 'Rua Alagoana', 685, 'Oliveirinha', 'Foz do Iguaçu', 'Ceará', '98556-974', 'CE', 14);
+INSERT INTO endereco VALUES (NULL, 'Avenida', 'Rua Namina', 553, 'Samboa', 'Juazeiro do Norte', 'Paraná', '68452-412', 'PR', 15);
+INSERT INTO endereco VALUES (NULL, 'Rua', 'Rua Olivia', 876, 'Caraceti', 'Sumaré', 'São Paulo', '72860-268', 'SP', 16);
+INSERT INTO endereco VALUES (NULL, 'Rua', 'Rua Antilope', 142, 'Marquita', 'Ipatinga', 'Minas Gerais', '74125-352', 'MG', 17);
+INSERT INTO endereco VALUES (NULL, 'Avenida', 'Rua Tabata', 3578, 'Londrina', 'Viamão', 'Rio Grande do Sul', '47865-685', '', 18);
+INSERT INTO endereco VALUES (NULL, 'Rua', 'Rua Oliveira', 246, 'Anbona', 'Imperatriz', 'Maranhão', '69850-274', 'MA', 19);
+INSERT INTO endereco VALUES (NULL, 'Avenida', 'Rua Ancas', 338, 'Santa Sebastina', 'São Carlos', 'São Paulo', '41256-896', 'SP', 20);
+INSERT INTO endereco VALUES (NULL, 'Avenida', 'Rua Polvina', 4441, 'São Jorge', 'Criciúma', 'Santa Catarina', '74528-335', 'SC', 21);
+INSERT INTO endereco VALUES (NULL, 'Avenida', 'Rua Polvina', 4441, 'São Jorge', 'Criciúma', 'Santa Catarina', '74528-335', 'SC', 21);
+INSERT INTO endereco VALUES (NULL, 'Avenida', 'Rua Polvina', 4441, 'São Jorge', 'Criciúma', 'Santa Catarina', '74528-335', 'SC', 21);
+INSERT INTO endereco VALUES (NULL, 'Rua', 'Rua Oliveira', 246, 'Anbona', 'Imperatriz', 'Maranhão', '69850-274', 'MA', 19);
+INSERT INTO endereco VALUES (NULL, 'Rua', 'Rua Dupla Sacada', 923, 'Baiacu', 'Itajaí', 'Santa Catarina', '36582-596', 'SC', 25);
